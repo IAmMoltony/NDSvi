a0cd7f80ea6e92925292e14604b90c1423b73e60
moltony <koronavirusnyj@gmail.com>
2023-07-12 19:06:21 +0600

    Change "Insert mode" to "-- INSERT --"

--- source/main.cpp
+++ source/main.cpp
@@ -184,7 +184,7 @@ int main(int argc, char **argv)
             }
             break;
         case editmodeInsert:
-            printf("Insert mode");
+            printf("-- INSERT --");
             break;
         }
 
